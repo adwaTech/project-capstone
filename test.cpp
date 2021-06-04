f98c8506be5160f21e5086a8f4162fbe7ad15f8f
mesi473 <78929337+mesi473@users.noreply.github.com>
2021-06-04 14:38:15 +0300

    "something is deleted"

--- test.cpp
+++ test.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main(){
   cout<<"hello world";
   cout<<"How are you today?";
-  cout<<"This is edited by messi";
   cout<<"roskee edited this in dorm 423";
   cout<<"I am messi";
   return 0;
