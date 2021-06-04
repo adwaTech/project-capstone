5a5ec2cfcecb37f229d92844e9f29f11dc4f42dc
Kirubel Adamu <kirubeladamu@gmail.com>
2021-06-04 20:50:39 +0300

    added fortune teller

--- test.cpp
+++ test.cpp
@@ -4,5 +4,9 @@ int main(){
   cout<<"hello world";
   cout<<"how are you, from roskee";
   cout<<"This is a new change roskee has made";
+  int x;
+  cout<<"Enter your age and I will tell you the day you will be famous!"<<endl;
+  cin>>x;
+  cout<<"You will be famous after "<<x%10<<" years! Congrats!!!";
   return 0;
 }
