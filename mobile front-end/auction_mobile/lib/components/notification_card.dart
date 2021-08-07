0ab28861e64dbb6dccfe04411de75c2fe0b302d3
kirubel adamu <kirubeladamu@gmail.com>
2021-08-07 10:31:19 +0300

    added your auctions page

--- mobile front-end/auction_mobile/lib/components/notification_card.dart
+++ mobile front-end/auction_mobile/lib/components/notification_card.dart
@@ -2,21 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NotificationCard extends StatelessWidget {
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Card(
       child: Container(
         height: 100,
         child: ListTile(
           isThreeLine: true,
           title: Text('Your auction is ready'),
-          subtitle: Text('Your auction is visible to customers. Check how it looks to customers'),
+          subtitle: Text(
+              'Your auction is visible to customers. Check how it looks to customers'),
           trailing: TextButton(
-            onPressed: (){},
+            onPressed: () {},
             child: Text('Mark as read'),
           ),
-          onTap: (){
-
-          },
+          onTap: () {},
         ),
       ),
     );
--- mobile front-end/auction_mobile/lib/your_auctions.dart
+++ mobile front-end/auction_mobile/lib/your_auctions.dart
@@ -1,7 +1,7 @@
+import 'package:auction_mobile/components/drawer.dart';
 import 'package:flutter/material.dart';
 
 class YourAuctions extends StatefulWidget {
-
   @override
   _YourAuctionsState createState() => _YourAuctionsState();
 }
@@ -9,8 +9,91 @@ class YourAuctions extends StatefulWidget {
 class _YourAuctionsState extends State<YourAuctions> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            title: Text('Your Auctions'),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => Card(
+                      elevation: 20,
+                      child: Container(
+                        height: 150,
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/product_logo.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                            Expanded(
+                                child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Container(
+                                        height: 150,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Text('Auction Title',
+                                                style: TextStyle(fontSize: 20)),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('Deadline')),
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('2013'))
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('Other Stuff')),
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('Other value'))
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('Other stuff')),
+                                                Expanded(
+                                                    flex: 1,
+                                                    child: Text('Other stuff'))
+                                              ],
+                                            ),
+                                            Expanded(
+                                                child: Row(children: [
+                                              Expanded(
+                                                  child: Align(
+                                                      alignment:
+                                                          Alignment.bottomRight,
+                                                      child: Padding(
+                                                          padding:
+                                                              EdgeInsets.all(
+                                                                  10),
+                                                          child: ElevatedButton(
+                                                              onPressed: () {},
+                                                              child: Text(
+                                                                  'Preview')))))
+                                            ]))
+                                          ],
+                                        ))))
+                          ],
+                        ),
+                      ),
+                    ),
+                childCount: 20),
+          )
+        ],
+      ),
     );
   }
 }
