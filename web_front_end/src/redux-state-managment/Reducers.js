5d0388b1840c484e3a2c8b090ee542a0561fe49b
mesi473 <meseretkifle2@gmail.com>
2021-08-11 23:06:35 +0300

    token is saved in cookie when the user is registered

--- web_front_end/src/redux-state-managment/Reducers.js
+++ web_front_end/src/redux-state-managment/Reducers.js
@@ -31,6 +31,11 @@ export const Register=(state=initialState,action)=>{
     switch(action.type){
         case Constant.REGISTER:
             if(action.payload.status===200){
+                var now = new Date();
+                var time = now.getTime();
+                var expireTime = time + 1000*36000;
+                now.setTime(expireTime);
+                document.cookie = `user=${action.payload.data.user}; token=${action.payload.data.token} ; expires=${now.toUTCString()}; path=/`;
                 return{
                     ...state,
                     user:action.payload.data.user,
